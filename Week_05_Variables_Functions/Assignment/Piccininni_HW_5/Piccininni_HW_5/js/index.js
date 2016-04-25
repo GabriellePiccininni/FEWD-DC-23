7841870f37f826755327e8b218b5f01fbf550bf8
Joni Halabi <her@thatdevgirl.com>
2016-04-24 22:06:42 -0400

    Homework 5 feedback

--- Week_05_Variables_Functions/Assignment/Piccininni_HW_5/Piccininni_HW_5/js/index.js
+++ Week_05_Variables_Functions/Assignment/Piccininni_HW_5/Piccininni_HW_5/js/index.js
@@ -1,5 +1,19 @@
+/* 
+	Hi Gaby!  I put my feedback on your code in Javascript comments like this one.
+
+	Overall, great job with this one!  Plus I'm glad you got the GitHub issues worked out.
+
+	I really like how you included your pseudocode as comments throughout the code.
+	More comments inline below.
+*/
+
 $(document).ready(function(){
 
+	/*
+		You do not need to declare your "city" variable up here, since you will not use it until the
+		cityBackground() function is called.  You can declare and assign the "city" variable at the same
+		time inside that function.
+	*/
 	var city;
 
 	$('form').submit(cityBackground);
@@ -9,10 +23,34 @@ $(document).ready(function(){
 	
 	function cityBackground(event){
 		event.preventDefault();
+
+		/*
+			As I mention above, you can declare and assign the "city" variable all at once here, like this:
+				var city = $('#city-type').val(); 
+		*/
 		city = $('#city-type').val(); 
 
 		console.log(city);
 
+			/*
+				Your individual conditional statements are great here, but don't forget that you
+				can use "else" to combine all of these if statements together.  That is a good way to
+				set a "default" action - for example, if someone types in a city that is not
+				one of the ones you are checking - that will reset the background image to that
+				first image you see when you first load the page (i.e. when you remove all of the 
+				classes from the body tag).  So, for example:
+
+				if (user chose NYC) {
+					// Show the NYC background
+				} else if (user chose SF) {
+					// Show the SF background
+				} else if (user chose LA) {
+					// Show the LA background
+				} else {
+					// Remove all classes from body to show the default background.
+					// This happens when the user chooses anything but NYC, SF, or LA
+				}
+			*/
 			if (city === "New York" || city === "NYC" ||  city === "New York City") {
 				$('body').removeClass();
 				$('body').addClass('nyc');
