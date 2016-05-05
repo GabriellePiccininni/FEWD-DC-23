42d1420488e254adbea980b5926f763ca940a1bc
Gabrielle Piccininni <piccingt@gmail.com>
2016-05-05 18:37:46 -0400

    homework 8 updates

--- Week_08_Responsive_Bootstrap/15_responsive_basics/starter_code/startup_matchmaker_responsive/js/main.js
+++ Week_08_Responsive_Bootstrap/15_responsive_basics/starter_code/startup_matchmaker_responsive/js/main.js
@@ -1,5 +1,6 @@
 document.getElementsByTagName("li")[0].addEventListener("click",function(){
 	if(!this.parentNode.getAttribute("class")){
+		//if li[0] does not have a class, add set the attribute and add the class 'slideDown'
 		this.parentNode.setAttribute("class","slideDown");
 	}
 	else{
